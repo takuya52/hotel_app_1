68fe06fa85326a19180d684f30af1c77ebd1d6cc
田辺拓也 <tanabetakuya@tanabetakuyanoMacBook-Pro.local>
2022-08-11 21:31:20 +0900

    ヘッダー並びの質問

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,10 +62,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   
 protected
 
-def update_resource(resource, params)
-  resource.update_without_current_password(params)
-end
-
 def after_update_path_for(resource) 
   users_profile_path
 end
