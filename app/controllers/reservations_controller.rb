b1a876dae1d619a8ad5f803aee597f720017c99e
田辺拓也 <tanabetakuya@tanabetakuyanoMacBook-Pro.local>
2022-08-15 05:52:28 +0900

    合計料金に関する質問

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,13 +4,32 @@ class ReservationsController < ApplicationController
   end
 
   def new
-    @room = Room.find(params[:room_id])
-    binding.pry
+    @room = Room.find(params[:id])
     @user_id = current_user.id
-    binding.pry
     @reservation = Reservation.new
   end
 
-  def show
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = current_user.id
+    @total_rate = @reservation.room.single_rate * @reservation.number_of_people * (@reservation.end_date - @reservation.start_date).to_i
+    if @reservation.save
+      flash[:notice] = "予約が完了しました"
+      redirect_to reservation_path(@reservation.id)
+    else
+      render :new
+    end
   end
+
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  private
+  
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :number_of_people, :room_id).merge(user_id: current_user.id)
+  end
+
 end
