12faba98ce9efdef1c2c7c92962ba599db89afab
田辺拓也 <tanabetakuya@tanabetakuyanoMacBook-Pro.local>
2022-08-18 19:29:29 +0900

    バリデーションに関する質問

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,19 @@
 class Reservation < ApplicationRecord
   belongs_to :room
   belongs_to :user
+
+  validate :start_end_check
+  validate :start_date_before_today
+
+  def start_end_check
+    if start_date.present? && end_date.present? && start_date >= end_date
+      errors.add(:end_date, "は開始日より後の日付を入力してください")
+    end
+  end
+  
+  def start_date_before_today
+    if start_date.present? && end_date.present? && start_date < Date.today
+     errors.add(:start_date, "は今日以降の日付で選択してください")
+    end 
+  end
 end
